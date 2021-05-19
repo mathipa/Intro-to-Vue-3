cb82ed234a6e12239afd646b3afcbfd62bd903be
Mathipa Jason Makgato <mathipa87@gmail.com>
2021-05-19 13:17:04 +0200

    computed properties

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             cart:0,
+            onSale: true,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 5 },
             ]
         }
     },
@@ -17,8 +17,28 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index;
+            console.log(this.selectedVariant);
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if(this.onSale) {
+                return this.brand + ' ' + this.product + ' ' + 'is on Sale';
+            }
+            return '';
+           
+        },
+        
     }
 })
